adf34cdd0ebe90881ce251ee41517c6de606a730
Joshua Bussdieker <jbussdieker@gmail.com>
2013-08-24 09:55:57 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BabyRecord::Application.config.session_store :cookie_store, key: '_baby-record_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BabyRecord::Application.config.session_store :active_record_store
